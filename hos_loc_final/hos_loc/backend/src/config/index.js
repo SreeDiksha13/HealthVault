2679f052f5b47e6c302fe11306d274cd2ec1b4b9
SreeDiksha13 <sreediksha45@gmail.com>
2025-11-20 20:47:25 +0530

    initial commit

--- hos_loc_final/hos_loc/backend/src/config/index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const config = {
-  port: process.env.PORT || 4000,
-  mongoUri: process.env.MONGO_URI,
-  googleApiKey: process.env.GOOGLE_PLACES_API_KEY,
-  overpassApi: process.env.OVERPASS_API,
-};
